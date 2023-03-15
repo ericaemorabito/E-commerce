c2dee07b029a1b20885b2a624f7ba4344ecd9122
Erica <ericaemorabito@gmail.com>
2023-03-15 13:09:38 -0700

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import colors from 'colors';
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import path from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -15,13 +16,24 @@ const app = express();
 // allows us to accept json data in req body
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running')
-});
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
+const __dirname = path.resolve();
+
+// When in production --> point to index.html file in build
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 // Error Middleware
 
@@ -34,4 +46,9 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 3001;
 
 // In tutorial this is actually 5000 --> and it is proxy 5000 in frontend/package.json proxy
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold
+  )
+);
